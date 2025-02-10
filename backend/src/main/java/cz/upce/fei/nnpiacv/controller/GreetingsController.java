2b8032f4530e226be08e75c895473d149b2b2512
mtupce <94901164+mtupce@users.noreply.github.com>
2025-02-10 15:25:00 +0100

    CV-01: Showcase of automatic constructor + cleanup

--- backend/src/main/java/cz/upce/fei/nnpiacv/controller/GreetingsController.java
+++ backend/src/main/java/cz/upce/fei/nnpiacv/controller/GreetingsController.java
@@ -2,14 +2,13 @@ package cz.upce.fei.nnpiacv.controller;
 
 
 import cz.upce.fei.nnpiacv.service.GreetingsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingsController {
 
-    private GreetingsService greetingService ;
+    private final GreetingsService greetingService ;
 
     public GreetingsController(GreetingsService greetingService) {
         this.greetingService = greetingService;
