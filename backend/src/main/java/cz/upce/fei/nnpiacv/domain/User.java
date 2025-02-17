f745430c4955d849b4c380f0cd9ebb576fac7f60
mtupce <94901164+mtupce@users.noreply.github.com>
2025-02-17 14:33:43 +0100

    CV-02: User class

---
+++ backend/src/main/java/cz/upce/fei/nnpiacv/domain/User.java
@@ -0,0 +1,50 @@
+package cz.upce.fei.nnpiacv.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class User {
+
+    private long id;
+    private String email;
+    private String password;
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+
+
+
+}
--- backend/src/main/java/cz/upce/fei/nnpiacv/service/GreetingsService.java
+++ backend/src/main/java/cz/upce/fei/nnpiacv/service/GreetingsService.java
@@ -1,10 +1,21 @@
 package cz.upce.fei.nnpiacv.service;
 
+import cz.upce.fei.nnpiacv.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingsService {
+    private Logger logger = LoggerFactory.getLogger(GreetingsService.class);
+
+
     public String sayGreeting(){
-        return "Hello VVorld";
+
+        User user = new User( 0L, "cool@email.cz", "heslo");
+        logger.info("USER CREATED: "+ user);
+
+        return user.toString();
     }
+
 }
